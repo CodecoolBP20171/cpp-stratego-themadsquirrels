1bd1e8d8d0b6039981fbdf29ec4b36f975e14c73
martonMeszaros <marton.m95@gmail.com>
2017-11-21 14:49:27 +0100

    ClickActionType enum class

---
+++ stratego/include/ClickActionType.h
@@ -0,0 +1,17 @@
+//
+// Created by meszi on 2017.11.21..
+//
+
+#ifndef STRATEGO_CLICKACTIONTYPE_H
+#define STRATEGO_CLICKACTIONTYPE_H
+
+enum class ClickActionType {
+    RESET_ACTION,
+    EXIT_ACTION,
+    NEXT_ACTION,
+    SETUP_ACTION,
+    BOARD_ACTION,
+    CONTAINER_ACTION
+};
+
+#endif //STRATEGO_CLICKACTIONTYPE_H
