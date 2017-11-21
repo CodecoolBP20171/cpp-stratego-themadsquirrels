3cb8ee248c35199f4de6b5cb2e3db5116233c3d2
martonMeszaros <marton.m95@gmail.com>
2017-11-21 14:47:12 +0100

    PieceType enum class

---
+++ stratego/include/PieceTypes.h
@@ -0,0 +1,25 @@
+//
+// Created by meszi on 2017.11.21..
+//
+
+#ifndef STRATEGO_PIECETYPES_H
+#define STRATEGO_PIECETYPES_H
+
+enum class PieceTypes {
+    FLAG,
+    SPY,
+    SCOUT,
+    MINER,
+    SERGANT,
+    LIEUTENANT,
+    CAPTAIN,
+    MAJOR,
+    COLONEL,
+    GENERAL,
+    MARSHALL,
+    BOMB,
+    EMPTY = 50,
+    WATER = 100
+};
+
+#endif //STRATEGO_PIECETYPES_H
