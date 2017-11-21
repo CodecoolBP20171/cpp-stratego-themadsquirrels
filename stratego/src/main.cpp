5b06e30425ee64b74d58b3cd1c6c39770b0e150d
martonMeszaros <marton.m95@gmail.com>
2017-11-21 15:44:04 +0100

    main loop
    Note: input for new game is undesigned yet.

--- stratego/src/main.cpp
+++ stratego/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "../include/Game.h"
+
+using namespace stratego;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Game game;
+
+    bool quit = false;
+    while (!quit) {
+        // Run a whole game from setup to finish.
+        switch (game.run()) {
+            case GameState::PLAYER_WIN:
+                // TODO: Congratulate and ask for new game input graphically.
+                std::string input;
+                std::cout << "Do you want to play again? (yes/no)" << std::endl;
+                std::cin >> input;
+                if (input != "yes") {
+                    quit = true;
+                }
+                break;
+            case GameState::RESET:
+                break;
+            case GameState::EXIT:
+                quit = true;
+                break;
+            default:
+                std::cout << "GameState returned from game.run() was unexpected!";
+                quit = true;
+        }
+    }
+
     return 0;
 }
