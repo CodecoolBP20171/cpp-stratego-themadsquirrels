f5166d80cb6875abad5c6207eed519a655286461
martonMeszaros <marton.m95@gmail.com>
2017-11-21 14:43:53 +0100

    GameState enum class

---
+++ stratego/include/GameState.h
@@ -0,0 +1,17 @@
+//
+// Created by meszi on 2017.11.21..
+//
+
+#ifndef STRATEGO_GAMESTATE_H
+#define STRATEGO_GAMESTATE_H
+
+enum class GameState {
+    PLAYER_SETUP,
+    PLAYER_TURN,
+    PLAYER_WIN,
+    SWITCHING,
+    RESET,
+    EXIT
+};
+
+#endif //STRATEGO_GAMESTATE_H
