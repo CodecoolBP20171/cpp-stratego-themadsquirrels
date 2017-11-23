a7430eb3a88e18a340301ca1a855b2d67a8af010
Bán Marcell <afga93+cc@gmail.com>
2017-11-23 18:52:54 +0100

    game logic vol.3
    playermove

--- stratego/include/PlayerMove.h
+++ stratego/include/PlayerMove.h
@@ -9,12 +9,13 @@
 #include "strategoTypes.h"
 #include "renderable/Player.h"
 #include "renderable/Selection.h"
+#include <renderable/clickable/displayMatrix/Board.h>
 
 namespace stratego {
     class PlayerMove {
     public:
         PlayerMove(sptr <Selection>& first, sptr <Selection>& second);
-        void execute();
+        void execute(sptr <Board>& board);
         const wptr<stratego::Player>& getPlayer() const;
         void setPlayer(sptr <stratego::Player>& player);
         bool isAttack() const;
@@ -23,9 +24,9 @@ namespace stratego {
         sptr<Selection> firstSelection;
         sptr<Selection> secondSelection;
         void drawMove();
-        void looseMove();
+        void looseMove(sptr<Board>& board);
         void marchMove();
-        void winMove();
+        void winMove(sptr <Board>& board);
     };
 }
 
